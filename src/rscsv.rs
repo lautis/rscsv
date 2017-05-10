1aac304053eaebb508aa2196fade35f80a503b03
Ville Lautanala <lautis@gmail.com>
2017-05-10 22:28:21 +0300

    Check types of array elements

--- src/rscsv.rs
+++ src/rscsv.rs
@@ -4,18 +4,25 @@ extern crate csv;
 
 use helix::sys;
 use helix::sys::VALUE;
-use helix::{UncheckedValue, CheckResult, CheckedValue, ToRust, ToRuby};
+use helix::{UncheckedValue, CheckResult, CheckedValue, ToRust};
 
 struct VecWrap<T>(Vec<T>);
 
-
-impl UncheckedValue<VecWrap<String>> for VALUE
-    where VALUE: UncheckedValue<String>
+impl<T> UncheckedValue<VecWrap<T>> for VALUE
+    where VALUE: UncheckedValue<T>
 {
-    fn to_checked(self) -> CheckResult<VecWrap<String>> {
+    fn to_checked(self) -> CheckResult<VecWrap<T>> {
         if unsafe { sys::RB_TYPE_P(self, sys::T_ARRAY) } {
             // TODO: Make sure we can actually do the conversions for the values.
-            Ok(unsafe { CheckedValue::<VecWrap<String>>::new(self) })
+            let len = unsafe { sys::RARRAY_LEN(self) };
+            let ptr = unsafe { sys::RARRAY_PTR(self) };
+            for i in 0..len {
+                let val = unsafe { *ptr.offset(i) };
+                if let Err(error) = val.to_checked() {
+                    return Err(format!("Failed to convert value for Vec<T>: {}", error));
+                }
+            }
+            Ok(unsafe { CheckedValue::<VecWrap<T>>::new(self) })
         } else {
             let val = unsafe { CheckedValue::<String>::new(sys::rb_inspect(self)) };
             Err(format!("No implicit conversion of {} into Vec<String>",
